68bc5486e5fbe6400aa8229ac7ce26386e10e73e
kerem <keremklks@gmail.com>
2025-11-18 15:45:31 +0300

    feat: implement navigation and contact list display in MainActivity

--- app/src/main/java/com/example/contactsapp/MainActivity.kt
+++ app/src/main/java/com/example/contactsapp/MainActivity.kt
@@ -11,20 +11,29 @@ import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
@@ -35,6 +44,7 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -49,12 +59,17 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import androidx.navigation.NavHost
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import androidx.room.Room
 import coil.compose.rememberAsyncImagePainter
 import com.example.contactsapp.ui.theme.ContactsAppTheme
 import com.example.contactsapp.ui.theme.GreenJC
 import com.example.jetcontactsapp.R
 import java.io.File
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -70,8 +85,93 @@ class MainActivity : ComponentActivity() {
         val viewModel: ContactViewModel by viewModels { ContactViewModelFactory(repository) }
 
         setContent {
-            
+            val navController = rememberNavController()
+            NavHost(navController = navController, startDestination = "contactList") {
+                composable("contactList") { ContactListScreen(viewModel, navController)}
+                composable("addContact") { AddContactScreen(viewModel, navController)}
+            }
         }
+    }
+}
+
+@Composable
+fun contactItem(contact: Contact, onClick: () -> Unit) {
+    Card(modifier = Modifier
+        .fillMaxWidth()
+        .padding(start = 12.dp, end = 12.dp, top = 8.dp, bottom = 8.dp)
+        .clickable(onClick = onClick),
+        colors = CardDefaults.cardColors(Color.White),
+        elevation = CardDefaults.cardElevation(4.dp)
+    ) {
+        Row(modifier = Modifier
+            .fillMaxWidth()
+            .clickable(onClick = onClick)
+            .padding(8.dp),
+            verticalAlignment = Alignment.CenterVertically) {
+            Image(painter = rememberAsyncImagePainter(contact.image),
+                contentDescription = contact.name,
+            modifier = Modifier
+                .size(50.dp)
+                .clip(CircleShape),
+            contentScale = ContentScale.Crop)
+            Spacer(modifier = Modifier.width(16.dp))
+            Text(contact.name)
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ContactListScreen(viewModel: ContactViewModel, navController: NavController){
+    val context = LocalContext.current.applicationContext
+
+    Scaffold(
+        topBar = {
+            TopAppBar(modifier = Modifier.height(48.dp),
+                title = {
+                    Box(modifier = Modifier
+                        .fillMaxHeight()
+                        .wrapContentHeight(Alignment.CenterVertically)) {
+                        Text("Contacts", fontSize = 18.sp)
+                    }
+                },
+                navigationIcon = {
+                    IconButton(onClick = {
+                        Toast.makeText(context, "Contacts", Toast.LENGTH_SHORT).show()
+                    }) {
+                        Icon(painter = painterResource(id = R.drawable.contacticon), contentDescription = null)
+                    }
+                }, colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = GreenJC,
+                    titleContentColor = Color.White,
+                    navigationIconContentColor = Color.White
+                ))
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                containerColor = GreenJC,
+                onClick = { navController.navigate("addContact") }
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.addcontact),
+                    contentDescription = "Add Contact",
+                    tint = Color.White
+                )
+            }
+        }
+    ) {paddingValues ->
+        val contacts by viewModel.allContacts.observeAsState(initial = emptyList())
+        LazyColumn(
+            modifier = Modifier.padding(paddingValues)
+        ) {
+            items(contacts){ contact ->
+                contactItem(contact = contact, onClick = {
+                    navController.navigate("contactDetail/${contact.id}")
+                })
+
+            }
+        }
+
     }
 }
 
